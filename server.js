9846efb029245ad184b6b969706c7752cba1ecda
Brian Emil Hartz <brianhartz@gmail.com>
2014-12-28 20:03:56 -0700

    initial commit, setup package, setup server

---
+++ server.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});

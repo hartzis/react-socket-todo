590d343b08f72228bff9cc45574ae6e38df29415
Brian Emil Hartz <brianhartz@gmail.com>
2014-12-30 20:32:32 -0700

    bundle front end js working, app working

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 /** @jsx React.DOM */
 
 var React = require('react');
-var todoApp = require('./components/TodoApp.react');
+var TodoApp = require('./components/TodoApp.react');
 
 // Snag the initial state that was passed from the server side
 var initialState = JSON.parse(document.getElementById('initial-state').innerHTML)
 
+console.log('initial-state-', initialState);
+
 // Render the components, picking up where react left off on the server
-React.renderComponent(
-  <TodoApp tweets={initialState}/>,
+React.render(
+  <TodoApp todos={initialState}/>,
   document.getElementById('react-app')
 );
